7f3e88f70461804afece0f5e9a3ae38d104be61d
jdietm <j.diet.m@gmail.com>
2022-05-20 12:00:53 -0400

    Updates Program

--- BugTracker/Program.cs
+++ BugTracker/Program.cs
@@ -1,4 +1,5 @@
 using BugTracker.Data;
+using BugTracker.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,13 +11,19 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<Users>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+using (var Scope = app.Services.CreateScope())
+{
+    var Services = Scope.ServiceProvider;
+    await SeedData.Initialize(Services);
+}
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
